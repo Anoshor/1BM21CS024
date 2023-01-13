bea050ce389c2dc661c47112d635e25df8c4fbf2
Anoshor <100086066+Anoshor@users.noreply.github.com>
2023-01-13 13:06:04 +0530

    Create pmain.java

---
+++ lab8/pmain.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import cie.internal;
+import see.external;
+
+class pmain {
+	public static void main(String xx[]) {
+		Scanner s=new Scanner(System.in);
+		
+		external b1= new external();
+		b1.calc();
+	}
+}
